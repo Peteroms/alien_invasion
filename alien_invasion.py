899a2e37451831ef0ca557cc6cf5ea97938881b0
Peteroms <omondipeter934@gmail.com>
2025-03-09 21:12:50 +0300

    importing,initialising

---
+++ alien_invasion.py
@@ -0,0 +1,34 @@
+### Creating a Pygame window and Responding to user input
+import sys
+
+import pygame
+
+from settings import Settings
+
+def run_game():
+    # Initialize game, settings, and screen object
+    pygame.init()  # Initilaizes background settings for Pygame to work
+    screen = pygame.display.set_mode((1200, 800)) # the arguments (1200, 800) are dimension tuples
+    pygame.display.set_caption("PETER'S INVASION GAME")
+
+    # Set the background colour
+    bg_colour = (150, 170, 230) # In pygame colours are RGB ranging from 0 to 255, (255, 0, 0) is red.
+
+
+    # Start the main loop for the game.
+    while True:
+
+        # Watch for keynoard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Redraw the screen during each pass through the loop.Fill the screen with bg colour
+        screen.fill(bg_colour)
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+run_game()  # Initializes the game and starts the main loop
+
+
